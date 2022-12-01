2de03da028eff15e4e69b0c44fef0580bbae5c92
Jeong_Min <102730775+123123445@users.noreply.github.com>
2022-12-01 14:38:43 +0900

    배경추가

--- My project/Assets/Scripts/MoveMap.cs
+++ My project/Assets/Scripts/MoveMap.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveMap : MonoBehaviour
-{
-    public float speed;
-    public float maxSpeed;
-
+{ 
     private void Update()
     {
         if (transform.position.x <= -21.79)
