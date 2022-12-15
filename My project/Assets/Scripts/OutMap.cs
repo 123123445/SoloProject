cba20eec79e6b9df9ad5b7115d06d5d19b579748
Jeong_Min <102730775+123123445@users.noreply.github.com>
2022-12-15 15:19:34 +0900

    메인 카메라 밖으로 나가면 체력달고 위치로 복귀

--- My project/Assets/Scripts/OutMap.cs
+++ My project/Assets/Scripts/OutMap.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class OutMap : MonoBehaviour
 {
+    public GameObject teleport;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            MainCharactor.instance.nowHp = 0;
+            MainCharactor.instance.isCanHit = false;
+            MainCharactor.instance.nowHp--;
+            MainCharactor.instance.gameObject.transform.position = teleport.transform.position;
         }
     }
 }
