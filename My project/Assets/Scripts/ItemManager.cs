17f38a28bdfce14afc1330003681c0453f9c704c
Jeong_Min <102730775+123123445@users.noreply.github.com>
2023-01-05 14:54:09 +0900

    item_manager

--- My project/Assets/Scripts/ItemManager.cs
+++ My project/Assets/Scripts/ItemManager.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class ItemManager : MonoBehaviour
 {
     public List<GameObject> itemList = new List<GameObject>();
+    public List<GameObject> pos = new List<GameObject>();
+    public float speed;
+
+    IEnumerator ItemCreate()
+    {
+        while (true)
+        {
+            if (MainCharactor.instance.isDie)
+            {
+                StopCoroutine("ItemCreate");
+            }
+            Instantiate(itemList[Random.Range(0, itemList.Count)], pos[Random.Range(0, pos.Count)].transform.position, Quaternion.identity);
+            speed = (Random.Range(1, 3) / GameManager.instance.speed) * 10;
+            yield return new WaitForSeconds(speed);
+        }
+    }
 }
